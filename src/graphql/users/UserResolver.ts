7d688c62f168e76ce23f36d420860cf9425a2fab
Jef Braem <jef.braem@codifly.be>
2021-02-10 23:16:49 +0100

    exercise 3

--- src/graphql/users/UserResolver.ts
+++ src/graphql/users/UserResolver.ts
@@ -1,69 +1,18 @@
-import {
-  Arg,
-  Authorized,
-  Ctx,
-  Field,
-  FieldResolver,
-  Mutation,
-  ObjectType,
-  Query,
-  Resolver,
-  ResolverInterface,
-  Root,
-  Subscription,
-} from 'type-graphql';
+import { Ctx, Query, Resolver } from 'type-graphql';
 import { Service } from 'typedi';
 
 import { Context } from '../../global/GraphQL';
-import BonusService from '../../services/BonusService';
 import UserService from '../../services/UserService';
-import LoginInput from './LoginInput';
 import User from './User';
 
-@ObjectType()
-class LoginReturn {
-  @Field()
-  token: string;
-}
-
 @Service()
-@Resolver(() => User)
-class UserResolver implements ResolverInterface<User> {
-  constructor(
-    private userService: UserService,
-    private bonusService: BonusService,
-  ) {}
+@Resolver()
+class UserResolver {
+  constructor(private userService: UserService) {}
 
-  @FieldResolver()
-  activeBonuses(@Root() user: User) {
-    return this.bonusService.getActiveBonusesForUser(user);
-  }
-
-  @Authorized()
   @Query(() => User)
   me(@Ctx() ctx: Context) {
     return this.userService.getUser(ctx.user!.id);
-  }
-
-  @Mutation(() => LoginReturn)
-  login(@Arg('input') args: LoginInput) {
-    return this.userService.login(args);
-  }
-
-  @Subscription(() => [User], {
-    topics: `USERS`,
-    description: 'Get live updates of all users',
-  })
-  usersLive() {
-    return this.userService.getUsers();
-  }
-
-  @Subscription(() => User, {
-    topics: ({ context }) => `USER_${context.user.id}`,
-    description: 'Get live updates for your user account.',
-  })
-  userLive(@Root() id: string) {
-    return this.userService.getUser(id);
   }
 }
 
