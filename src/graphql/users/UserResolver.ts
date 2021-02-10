4fbdc991d4016792af5e162656f43277e3fb21b1
Jef Braem <jef.braem@codifly.be>
2021-02-10 23:25:11 +0100

    exercise 5

--- src/graphql/users/UserResolver.ts
+++ src/graphql/users/UserResolver.ts
@@ -1,9 +1,37 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import LumberyardService from 'src/services/LumberyardService';
+import {
+  Arg,
+  Authorized,
+  Ctx,
+  Field,
+  FieldResolver,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  ResolverInterface,
+  Root,
+} from 'type-graphql';
 import { Service } from 'typedi';
 
 import { Context } from '../../global/GraphQL';
 import UserService from '../../services/UserService';
 import User from './User';
+@InputType()
+class LoginInput implements Partial<User> {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class LoginReturn {
+  @Field()
+  token: string;
+}
 
 @Service()
 @Resolver()
@@ -13,6 +41,11 @@ class UserResolver {
   @Query(() => User)
   me(@Ctx() ctx: Context) {
     return this.userService.getUser(ctx.user!.id);
+  }
+
+  @Mutation(() => LoginReturn)
+  login(@Arg('input') args: LoginInput) {
+    return this.userService.login(args);
   }
 }
 
